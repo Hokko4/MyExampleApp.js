44a881b1e84fc1cf7acec3f50112a6133a8e6968
horikoshi <horikoshi.seraku@gmail.com>
2017-12-19 09:41:36 +0900

    微修正

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,20 +3,23 @@
 $(function() {
   // ハンバーガーメニュー実装
   $('#hamburger').click(function() {
-    // $('nav').show();
     if ($('nav').hasClass('close')) {
+      // ハンバーガーを右に移動
       $('#hamburger').stop().animate({
-        left: '200px'
+        left: '215px'
       }, 1000, 'swing');
+      // ナビゲーションを右に移動
       $('nav').stop().animate({
         left: '0'
       }, 1000, 'swing');
       $('nav').toggleClass('close');
     } else {
+      // ハンバーガーを左に移動（閉じる）
       $('#hamburger').stop().animate({
         left: '15px',
         top: '15px'
       }, 1000, 'swing');
+      // ナビゲーションを左に移動（閉じる）
       $('nav').stop().animate({
         left: '-200px'
       }, 1000, 'swing');
