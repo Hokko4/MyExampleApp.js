af52ec4b89224587a7619dd7f8b8bfc201fbd2c1
horikoshi <horikoshi.seraku@gmail.com>
2017-12-20 18:18:39 +0900

    button slide 途中

--- js/auto-slide.js
+++ js/auto-slide.js
@@ -2,17 +2,17 @@
 
 $(function() {
   // スライドショー実装
-  $('.slide ul li').hide();
-  $('.slide ul li:first-child').show();
+  $('.slide > ul li').hide();
+  $('.slide > ul li:first-child').show();
 
   var interval = 3000;
   var speed = 1000;
-  console.log($('.slide ul li:last-child').length);
+  console.log($('.slide > ul li:last-child').length);
 
   // interval id
   var intId = setInterval(function() {
     var active = $('.active');
-    var next = $('.active').next().length !== 0 ? $('.active').next() : $('.slide ul li:first-child');
+    var next = $('.active').next().length !== 0 ? $('.active').next() : $('.slide > ul li:first-child');
     console.log(next);
     active.fadeOut(speed).removeClass('active');
     next.fadeIn(speed).addClass('active');
---
+++ js/button-slide.js
@@ -0,0 +1,7 @@
+'use strict'
+
+$(function() {
+  // スライドショー実装
+  $('.slide > ul li').hide();
+  $('.slide > ul li:first-child').show();
+});
