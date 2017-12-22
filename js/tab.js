b3a13862ff3c0bbf7b6e4a95b0dc29319a7caa46
horikoshi <horikoshi.seraku@gmail.com>
2017-12-22 10:41:04 +0900

    add tab change

--- js/tab.js
+++ js/tab.js
@@ -1,5 +1,23 @@
 'use strict'
 
 $(function() {
-  
+  // タブクリック実装
+
+  $('.contents > ul li:first-child').addClass('active');
+  $('.tab-box .tab:first-child').addClass('active');
+
+  $('.contents > ul li').click(function() {
+    var index = $('.contents > ul li').index(this);
+    if ($(this).hasClass('active')) {
+      return false;
+    }
+
+    $('.contents > ul li').removeClass('active');
+    $(this).addClass('active');
+
+    $('.tab-box .active').hide().removeClass('active');
+    $('.tab-box .tab').eq(index).fadeToggle(1000).addClass('active');
+
+    return false;
+  });
 });
