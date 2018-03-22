ed8cf46f6546b12d1aa7ca30bfdda5a4f3cdf3b4
Hokko4 <wondermelon0715@gmail.com>
2018-03-22 21:12:47 +0900

    remove comment

--- js/modal.js
+++ js/modal.js
@@ -4,9 +4,6 @@ $(function() {
   //モーダルウィンドウの実装
   // 開くとき
   $('#open').click(function() {
-    // $('#modal').css('display', 'block');
-    // $('body').css('background-color', '#000');
-    // $('body').append('<div id="layer"></div>');
     $('#layer').show();
     $('#layer').toggleClass('active');
     $('#modal').toggleClass('active');
